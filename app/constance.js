cf152383ececebd1d79be55f41ed3db80af0564c
Prachi <prachibabar6@gmail.com>
2026-01-12 00:45:28 +0530

    chnges

--- app/constance.js
+++ app/constance.js
@@ -1,7 +1,7 @@
 const CONFIG={
     // API_BASE_URL:"http://localhost:5000/api",
     // API_BASE_URL:"http://69.62.84.113:5016/api",
-    API_BASE_URL :"https://api.astrioninnovation.com",
-}
+    API_BASE_URL :"https://api.astrioninnovation.com/api",
+};
 
 export default CONFIG
